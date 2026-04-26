219001a2514f695fca5a8fe77c67b53479688b33
LuiLawrance <lawranceluI@gmail.com>
2026-04-25 23:27:56 -0700

    👤 Users Implemented (#1)
    ℹ️ Users can now create logins and login into the system.
    
    ✅ Additions:
    + Created "user.py" to handle all user login related functions
    + Users can now login by providing the a "username" + "password"
    + Users can create new logins
    + Some additional User IDE configs
    
    ❗ Notes:
    -> A reset/change password option has not been implemented yet and will be tackled later

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
-import file
+import sys
+import user
 
 
 def main():
-    file.new_json("DATA_CLIENT/GA_CARDS/GA_CARDS")
+    print("\n=== Grand Archive Library ===")
+
+    while True:
+        input_user = input(
+            "\nEnter your username, 1 to create a new user, or 0 to exit: "
+        ).strip()
+
+        if input_user == "0":
+            print("Exiting program.")
+            sys.exit()
+
+        elif input_user == "1":
+            username = input("Enter new username: ").strip()
+
+            while not username:
+                print("Username cannot be empty.")
+                username = input("Enter new username: ").strip()
+
+            while user.check_user(username):
+                print("Username already exists.")
+                username = input("Enter new username: ").strip()
+
+            password = input("Enter your password: ").strip()
+            user.new_user(username, password)
+
+        else:
+            if user.check_user(input_user):
+                input_password = input("Enter your password: ").strip()
+                current_user = user.user_login(input_user, input_password)
+
+                if current_user:
+                    while True:
+                        user_input = input("You have reached the end. Please enter 0 to exit: ")
+
+                        if user_input == "0":
+                            break
+                        else:
+                            print("The only available option is 0")
+
+            else:
+                print("User does not exist.")
 
 
 if __name__ == '__main__':
